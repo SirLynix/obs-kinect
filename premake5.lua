e087a8b086fe77069a9b3e78e556c06a9ea21ad2
Lynix <lynix680@gmail.com>
2020-02-05 14:16:56 +0100

    Improve x86 support

--- premake5.lua
+++ premake5.lua
@@ -22,7 +22,7 @@ workspace("obs-kinect")
 		kind("SharedLib")
 		language("C++")
 		cppdialect("C++17")
-		targetdir("bin/%{cfg.buildcfg}")
+		targetdir("bin/%{cfg.buildcfg}/%{cfg.architecture}")
 
 		files({ "src/**.hpp", "src/**.c", "src/**.cpp" })
 
@@ -31,26 +31,44 @@ workspace("obs-kinect")
 		links("kinect20")
 		links("obs")
 
-		filter({ "action:vs*" })
+		filter("action:vs*")
 			defines("_ENABLE_ATOMIC_ALIGNMENT_FIX")
 
-		filter({ "platforms:x86" })
+		filter("platforms:x86")
 			architecture "x32"
 			libdirs(assert(Config.libkinect.Lib32, "Missing kinect lib dir (x86)"))
 			libdirs(assert(Config.libobs.Lib32, "Missing obs lib dir (x86)"))
 
-		filter({ "platforms:x86_64" })
+		filter("platforms:x86_64")
 			architecture "x64"
 			libdirs(assert(Config.libkinect.Lib64, "Missing kinect lib dir (x86_64)"))
 			libdirs(assert(Config.libobs.Lib64, "Missing obs lib dir (x86_64)"))
 
-
 		filter("configurations:Debug")
-			defines({ "DEBUG" })
+			defines("DEBUG")
 			symbols("On")
-			postbuildcommands({ "{COPY} %{cfg.buildtarget.abspath} " .. Config.CopyToDebug })
 
 		filter("configurations:Release")
-			defines({ "NDEBUG" })
-			optimize("On")
-			postbuildcommands({ "{COPY} %{cfg.buildtarget.abspath} " .. Config.CopyToRelease })
+			defines("NDEBUG")
+			omitframepointer("On")
+			optimize("Full")
+
+		if (Config.CopyToDebug32) then
+			filter("configurations:Debug", "architecture:x32")
+				postbuildcommands({ "{COPY} %{cfg.buildtarget.abspath} " .. Config.CopyToDebug32 })
+		end
+
+		if (Config.CopyToDebug64) then
+			filter("configurations:Debug", "architecture:x64")
+				postbuildcommands({ "{COPY} %{cfg.buildtarget.abspath} " .. Config.CopyToDebug64 })
+		end
+
+		if (Config.CopyToRelease32) then
+			filter("configurations:Release", "architecture:x32")
+				postbuildcommands({ "{COPY} %{cfg.buildtarget.abspath} " .. Config.CopyToRelease32 })
+		end
+
+		if (Config.CopyToRelease64) then
+			filter("configurations:Release", "architecture:x64")
+				postbuildcommands({ "{COPY} %{cfg.buildtarget.abspath} " .. Config.CopyToRelease64 })
+		end
