6e61f7727e75c0168967dc8b5818b24d06148aeb
Jérôme Leclercq <lynix680@gmail.com>
2021-12-04 00:02:08 +0100

    Update copyright date

--- src/obs-kinect-sdk20/KinectSdk20Plugin.cpp
+++ src/obs-kinect-sdk20/KinectSdk20Plugin.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
-	Copyright (C) 2020 by Jérôme Leclercq <lynix680@gmail.com>
+	Copyright (C) 2021 by Jérôme Leclercq <lynix680@gmail.com>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
